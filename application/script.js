410909c0afe8962317b453f20f766c0429bd9a8e
Isael Lizama <isaelwebdeveloper@gmail.com>
2016-10-02 14:43:28 -0600

    changed squares

--- application/script.js
+++ application/script.js
@@ -1,18 +1,19 @@
-var rows = 8;
-var columns = 8;
-var $row = $("<div />",{
-  class = "row"
-
+var rows = 16;
+var columns = 16;
+var $row = $("<div />", {
+    class: 'row'
 });
-var $square = $("<div />",{
-
-
+var $square = $("<div />", {
+    class: 'square'
 });
-$(document).ready(function(){
-  for (var i = 0; i < $columns; i++) {
-      $row.append($square.clone());
-  }
-  for (var i = 0; i < rows; i++) {
-      $(".wrapper").append($row.clone());
-  }
+
+$(document).ready(function () {
+    //add columns to the the temp row object
+    for (var i = 0; i < columns; i++) {
+        $row.append($square.clone());
+    }
+    //clone the temp row object with the columns to the wrapper
+    for (var i = 0; i < rows; i++) {
+        $("#wrapper").append($row.clone());
+    }
 });
