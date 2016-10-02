ed981861346aed0ceb97b074656f2c8b07ce3305
Isael Lizama <isaelwebdeveloper@gmail.com>
2016-10-02 13:59:54 -0600

    did a whole change with some code found online

--- application/script.js
+++ application/script.js
@@ -1,19 +1,18 @@
 var rows = 8;
 var columns = 8;
-var $row = $("<div />", {
-    class: 'row'
-});
-var $square = $("<div />", {
-    class: 'square'
-});
+var $row = $("<div />",{
+  class = "row"
 
-$(document).ready(function () {
-    //add columns to the the temp row object
-    for (var i = 0; i < columns; i++) {
-        $row.append($square.clone());
-    }
-    //clone the temp row object with the columns to the wrapper
-    for (var i = 0; i < rows; i++) {
-        $("#wrapper").append($row.clone());
-    }
+});
+var $square = $("<div />",{
+
+
+});
+$(document).ready(function(){
+  for (var i = 0; i < $columns; i++) {
+      $row.append($square.clone());
+  }
+  for (var i = 0; i < rows; i++) {
+      $(".wrapper").append($row.clone());
+  }
 });
